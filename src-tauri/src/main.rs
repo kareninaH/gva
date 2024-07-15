cd8317bf0e283745b918102583a11bda0e95d060
kareninaH <kareninaH@githubcom>
2024-07-15 16:20:23 +0800

    push github

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,16 +2,25 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn greet(name: &str) -> Result<String, String> {
+    if name == "".to_string() {
+        return Err("参数不能为空".into());
+    }
+    Ok(format!("Hello, {}! You've been greeted from Rust!", name))
 }
+
+#[tauri::command]
+fn tauri_version() -> String {
+    "当前系统版本为: 1.0".to_string()
+}
+
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, tauri_version])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-    // init database 
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,14 +1,26 @@
 <script setup>
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api";
 
 const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  invoke("greet", { name: name.value })
+    .then((message) => {
+      console.log(message)
+      greetMsg.value = message
+    })
+    .catch((error) => console.error(error))
 }
+
+async function tauri_version() {
+  invoke("tauri_version")
+  .then((message) => {
+    console.log(message);
+  })
+}
+
 </script>
 
 <template>
@@ -16,6 +28,8 @@ async function greet() {
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
     <button type="submit">Greet</button>
   </form>
+  <br>
+  <button style="width: 150px; margin-left: 35%;" v-cloak="tauri_version">获取系统版本</button>
 
   <p>{{ greetMsg }}</p>
 </template>
